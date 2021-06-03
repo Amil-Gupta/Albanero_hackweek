c4633054dbc3b7fe7ee47c1b4654ca3ed9e3f009
Amil Utkarsh Gupta <64407800+Amil-Gupta@users.noreply.github.com>
2021-06-03 19:20:07 +0530

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,curr_depth=-1,max_depth=-1;
+    printf("Enter maximum number of characters\n");
+    scanf("%d",&n);
+    char s[n];
+    for(i=0;i<n;i++)
+    {
+        s[i]="";
+    }
+    printf("Enter string\n");
+    getchar();
+    scanf("%[^\n]s",&s);
+    for(i=0;i<n;i++)
+    {
+        if(s[i]=='(')
+        {
+            curr_depth++;
+        }
+        else if(s[i]==')')
+        {
+            curr_depth--;
+        }
+        if(curr_depth>max_depth)
+        {
+            max_depth=curr_depth;
+        }
+    }
+    //thought of adding a condition to check curr_depth=-1, but constraints say that s is guaranteed to be a VPS
+    printf("%d\n",max_depth);
+    return 0;
+}
