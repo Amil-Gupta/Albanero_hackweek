d354d2e919bdc1274be60c82ca4b226beb58df41
Amil Utkarsh Gupta <64407800+Amil-Gupta@users.noreply.github.com>
2021-06-04 15:53:15 +0530

    Alternative approach to question 7

---
+++ q7b.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,K,i,j,lpos0=-1,lmax=0,lcurr=0,count0;
+    printf("Enter no. of elements\n");
+    scanf("%d",&n);
+    int a[n],a1[n],pos0[n];
+    printf("Enter array elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Enter K\n");
+    scanf("%d",&K);
+    for(i=0;i<n;i++)
+    {
+        count0=0;
+        if(a[i]==1)
+        {
+            lcurr++;
+        }
+        else
+        {
+            for(j=i;j<n && count0<=K;j++)
+            {
+                if(a[j]==0)
+                {
+                    count0++;
+                }
+            }
+            lcurr+=j-i-1;
+            if(lcurr>lmax)
+            {
+                lmax=lcurr;
+            }
+            lcurr=0;
+        }
+    }
+    printf("%d\n",lmax);
+    return 0;
+}
