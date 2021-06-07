df3c3fb1bd1b25e06aee1f49a4172a7f0144a982
Amil Utkarsh Gupta <64407800+Amil-Gupta@users.noreply.github.com>
2021-06-07 12:53:48 +0530

    Add files via upload

--- q36.c
+++ q36.c
@@ -26,3 +26,4 @@ int main()
 //So, n-1 permutations are possible
 //Next, no. of steps=2 and (n-2)*(n-3)/2 permutations are possible (divided by 2 due to repetition of elements)
 //Similarly, we get (n-3)*(n-4)*(n-5)/3 , (n-4)*(n-5)*(n-6)*(n-7)/4 , ...
+//Finally, steps will be 2222...2 or 2222...21, which will have n/2 or n/2+1 elements, thus we will have made n replacements in total
